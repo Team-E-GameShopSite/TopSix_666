cfbc2993216a9e93bd03a13963937b0c9ddde20c
mooonakira <moonfoot0301@gmail.com>
2022-11-25 12:19:13 +0900

    拡張子をPHPに変更しました

--- kaihatuCART.php
+++ kaihatuCART.php
@@ -3,7 +3,7 @@
 <head>
 <meta content="text/html; charset=UTF-8" http-equiv="Content=Type">
 <style type="text/css"></style>
-<link href="styleERROR.css" rel="stylesheet" type="text/css">
+<link href="styleCART.css" rel="stylesheet" type="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -15,25 +15,28 @@ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jI
   <div class="header">
   <nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="kaihatuTOP.html">
+      <a class="navbar-brand" href="kaihatuTOP.php">
         <img src="img/TopSix.png" alt="" width="85" height="70">
       </a>
-      <form class="d-flex" action="kaihatuITIRAN.html">
+      <form class="d-flex" action="kaihatuITIRAN.php">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-secondary me-2" type="submit"><i class="bi bi-search"></i>Search</button>
-            <a  href="kaihatuCART.html"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-cart"></i>cart</button></a>
-            <a  href="kaihatuOKINIIRI.html"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-star"></i>favorite</button></a>
-            <a  href="kaihatuLOGIN.html"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-person-circle"></i>login</button></a>
+            <a  href="kaihatuCART.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-cart"></i>cart</button></a>
+            <a  href="kaihatuOKINIIRI.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-star"></i>favorite</button></a>
+            <a  href="kaihatuLOGIN.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-person-circle"></i>login</button></a>
       </form>
     </div>
   </nav>
   </div>
 
-  <p class="fs-1">404  ERROR</p>
-  <p class="fs-1">ページが見つかりません</p>
-  <div class="d-grid gap-2">
-  <a href="kaihatuTOP.html"  class="TOPNIMODORU"><button class="btn btn-outline-primary  me-2"   type="button">TOPに戻る→</button></a>
+  <p class="fs-1">ショッピングカート</p>
+  <div class="OKINIIRI">
+  <p class="fs-1">現在カートには何も入っていません</p>
   </div>
+  <div class="kounyuu">
+  <a href="kaihatuKOUNYU.php"  class="TOPNIMODORU"><button class="btn btn-outline-primary  btn-lg"   type="button">購入手続きへ→</button></a>
+  </div>
+  
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
--- kaihatuOKINIIRI.php
+++ kaihatuOKINIIRI.php
@@ -3,7 +3,7 @@
 <head>
 <meta content="text/html; charset=UTF-8" http-equiv="Content=Type">
 <style type="text/css"></style>
-<link href="styleCART.css" rel="stylesheet" type="text/css">
+<link href="styleOKINIIRI.css" rel="stylesheet" type="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -15,28 +15,27 @@ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jI
   <div class="header">
   <nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="kaihatuTOP.html">
+      <a class="navbar-brand" href="kaihatuTOP.php">
         <img src="img/TopSix.png" alt="" width="85" height="70">
       </a>
-      <form class="d-flex" action="kaihatuITIRAN.html">
+      <form class="d-flex" action="kaihatuITIRAN.php">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-secondary me-2" type="submit"><i class="bi bi-search"></i>Search</button>
-            <a  href="kaihatuCART.html"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-cart"></i>cart</button></a>
-            <a  href="kaihatuOKINIIRI.html"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-star"></i>favorite</button></a>
-            <a  href="kaihatuLOGIN.html"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-person-circle"></i>login</button></a>
+            <a  href="kaihatuCART.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-cart"></i>cart</button></a>
+            <a  href="kaihatuOKINIIRI.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-star"></i>favorite</button></a>
+            <a  href="kaihatuLOGIN.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-person-circle"></i>login</button></a>
       </form>
     </div>
   </nav>
   </div>
 
-  <p class="fs-1">ショッピングカート</p>
+  <p class="fs-1">お気に入り</p>
   <div class="OKINIIRI">
-  <p class="fs-1">現在カートには何も入っていません</p>
+  <p class="fs-1">お気に入りに登録したものはありません</p>
   </div>
-  <div class="kounyuu">
-  <a href="kaihatuKOUNYU.html"  class="TOPNIMODORU"><button class="btn btn-outline-primary  btn-lg"   type="button">購入手続きへ→</button></a>
+  <div class="TOPNIMODORU">
+  <a href="kaihatuTOP.php"  class="TOPNIMODORU"><button class="btn btn-outline-primary  btn-lg"   type="button">TOPに戻る→</button></a>
   </div>
-  
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
