d301332ee035c1b8e8b934619679583f9ff4d040
mooonakira <moonfoot0301@gmail.com>
2022-11-28 11:03:25 +0900

    確認画面を作りました

--- TOUROKUkakunin.php
+++ TOUROKUkakunin.php
@@ -3,7 +3,7 @@
 <head>
 <meta content="text/html; charset=UTF-8" http-equiv="Content=Type">
 <style type="text/css"></style>
-<link href="styleERROR.css" rel="stylesheet" type="text/css">
+<link href="stylekakunin.css" rel="stylesheet" type="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -12,19 +12,53 @@ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jI
 </head>
 
 <body>
+<div class="header">
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="kaihatuTOP.php">
+        <img src="img/TopSix.png" alt="" width="85" height="70">
+      </a>
+      <form class="d-flex" action="kaihatuITIRAN.php">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-secondary me-2" type="submit"><i class="bi bi-search"></i>Search</button>
+        <a  href="kaihatuCART.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-cart"></i>cart</button></a>
+        <a  href="kaihatuOKINIIRI.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-star"></i>favorite</button></a>
+        <a  href="kaihatuLOGIN.php"><button class="btn btn-outline-primary me-2" type="button"><i class="bi bi-person-circle"></i>login</button></a>
+      </form>
+    </div>
+  </nav>
+  </div>
+  <h1 class="touroku">以下の内容で登録します。よろしいですか？</h1>
+  <h1 class="center">
   <?php
+    echo"氏名<br>"
+    .$_POST['simei']."<br><br>";
 
-    echo "　　　　　氏名　　　　　　　　　　　　　".$_POST['simei']."<br><br>";
+    echo"氏名(フリガナ)<br>"
+    .$_POST['huri']."<br><br>";
 
-    echo "　　　　氏名(フリガナ)　　　　　　　　　　　".$_POST['huri']."<br><br>";
+    echo"メールアドレス<br>"
+    .$_POST['meado']."<br><br>";
 
-    echo "メールアドレス　　　　　　　　".$_POST['meado']."<br><br>";
+    echo"パスワード<br>"
+    .$_POST['pass']."<br><br>";
 
-    echo "　　パスワード　　　　　　　　　　".$_POST['pass']."<br><br>";
+    echo"郵便番号<br>"
+    .$_POST['yuubin']."<br><br>";
 
-    echo "パスワード（確認用）　　　　　".$_POST['yuubin']."<br><br>";
+    echo"住所<br>"
+    .$_POST['jyusyo']."<br><br>";
+
+    echo"電話番号<br>"
+    .$_POST['callnumber']."<br><br>";
+
+    echo"生年月日<br>"
+    .$_POST['birthday']."<br><br>";
 
    ?>
+   <a href="kaihatuTOUROKU.php">戻る</a>
+   <a href="kaihatuTOP.php">登録します</a>
+   </h1>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
