679837f66c90ca28262d965cf1363ee7b7e72835
taketakebamboo <takeuguisu@gmail.com>
2022-11-30 11:00:37 +0900

    dao/DBManager ファイルに全ての商品の情報を受け取れるDAOを追加しました。うまく使ってください。

--- dao/DBManager.php
+++ dao/DBManager.php
@@ -1,16 +1,27 @@
 <?php
 class DBManager{
-  $pdo = new PDO('mysql:host=mysql209.phy.lolipop.lan;dbname=LAA1418145-topsix666;charset=utf8','LAA1418145','topsix666');
+  private function dbConnect(){
+    $pdo = new PDO('mysql:host=mysql209.phy.lolipop.lan;dbname=LAA1418145-topsix666;charset=utf8','LAA1418145','topsix666');
 
-  return $pdo;
+    return $pdo;
+  }
+
+  public function databaseTest(){
+    $sql = 'INSERT INTO helloDB (text)
+            VALUES (?)';
+    $ps = $pdo->prepare($sql);
+    $ps->bindValue(1,$_POST['text'],PDO::PARAM_STR);
+    $ps->execute();
+  }
+
+  public function getItemTblAll(){
+    $pdo = $this->dbConnect();
+    $sql = "SELECT * FROM items_tbl";
+    $ps = $pdo->prepare($sql);
+    $ps->execute();
+    $searchArray = $ps->fetchAll();
+    return $searchArray;
+  }
 }
-
-$sql = 'INSERT INTO helloDB (text)
-        VALUES (?)';
-
-$ps = $pdo->prepare($sql);
-$ps->bindValue(1,$_POST['text'],PDO::PARAM_STR);
-$ps->execute();
-
 
 ?>
