fd3e4b1b0c6b5e319167aad36a2a4218b76b764d
taketakebamboo <takeuguisu@gmail.com>
2022-12-03 23:23:51 +0900

    確認用コミット

--- kaihatuITIRAN.php
+++ kaihatuITIRAN.php
@@ -112,10 +112,10 @@
           "<div class='col-md-2'>
             <div class='card'>
               <a href='kaihatuSYOUSAI.php' style='text-decoration: none;color:black'>
-                <img src='img/soft/pokemon/pokemon(2).jpg' class='card-img' alt='...' height='170'>
+                <img src=" . $row['image_path'] . " class='card-img' alt='...' height='170'>
                 <div class='card-body'>
-                  <h5 class='card-title'>ポケットモンスター スカーレット -Switch (【早期購入特典】プロモカード「ピカチュウ」 ×1 同梱)</h5>
-                  <h5 class='price'>6578円</h5>
+                  <h5 class='card-title'>" . $row['item_name'] . "</h5>
+                  <h5 class='price'>" . $row['item_price'] . "</h5>
               </a>
             </div>
           </div>";
