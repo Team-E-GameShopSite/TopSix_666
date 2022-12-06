ae2ad4829fbc105c01ae4a705ed8866671f6e994
taketakebamboo <takeuguisu@gmail.com>
2022-12-06 14:24:26 +0900

    一覧画面編集中3日目1

--- kaihatuITIRAN.php
+++ kaihatuITIRAN.php
@@ -118,7 +118,7 @@
                 <img src=" . $row['image_path'] . " class='card-img' alt='...' height='170'>
                 <div class='card-body'>
                   <h5 class='card-title'>" . $row['item_name'] . "</h5>
-                  <h5 class='price'>" . $row['item_price'] . "円</h5>
+                  <h5 class='price'>" . $row['item_price'] . "円</h5>"
         if ($flag == 0) {
           echo "</div>";
         }
