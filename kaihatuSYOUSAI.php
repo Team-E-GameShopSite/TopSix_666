c3452fea96920b46dee31dc308a4b046141450d6
taketakebamboo <takeuguisu@gmail.com>
2022-12-07 23:41:55 +0900

    詳細画面でクリックした商品を表示するようにしたい2

--- kaihatuSYOUSAI.php
+++ kaihatuSYOUSAI.php
@@ -39,6 +39,7 @@ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jI
     
       $searchItem = $dbmng->GetItemInfoToID($_GET['item_id']);
     
+      echo $_GET['item_id'];
       if(!empty($searchItem)){
         foreach($searchItem as $row){
           echo "<div class='cartcheck'>
