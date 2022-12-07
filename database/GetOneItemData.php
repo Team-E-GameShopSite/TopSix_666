a4fc57e84f4a909199360aced5a3ee6704c893c1
taketakebamboo <takeuguisu@gmail.com>
2022-12-07 23:58:24 +0900

    詳細画面でクリックした商品を表示するようにしたいテスト3

--- database/GetOneItemData.php
+++ database/GetOneItemData.php
@@ -6,7 +6,7 @@ $sql = "SELECT * FROM item_tbl WHERE item_id = 1";
 $ps = $pdo->prepare($sql);
 $ps->execute();
 
-$searchItem = $sp->fetchAll();
+$searchItem = $ps->fetchAll();
 
 foreach($searchItem as $row){
   echo $row['item_id'];
