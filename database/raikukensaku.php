aec14d777b3214b79c873a2b7b5b28c0843169b8
mobutteru <2101144@s.asojuku.ac.jp>
2022-12-07 11:10:11 +0900

    raikukennsakuファイルのインプットwoしましたを

--- database/raikukensaku.php
+++ database/raikukensaku.php
@@ -1,11 +1,11 @@
-<form action = "raikukensaku3.php" method = "POST">
+<form action = "raikukensaku3.php" method = "post">
     <input type = "text" name = "ken"><br>
     <input type = "submit" value = "検索">
 </form>
 
 <?php
 $pdo = new PDO('mysql:host=mysql209.phy.lolipop.lan;dbname=LAA1418145-topsix666;charset=utf8','LAA1418145','topsix666');
-$sql = $sql = "SELECT * FROM item_tbl WHERE item_name LIKE ? ORDER BY item_id DESC";
+$sql = "SELECT * FROM item_name ORDER BY item_id DESC";
 $selectData = $pdo -> quary($sql);
 
 foreach($selectData as $row){
