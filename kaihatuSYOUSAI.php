eecca3fe7eb5d0743fa7c8a7e27495c8f8ea1ab8
mooonakira <moonfoot0301@gmail.com>
2022-12-09 14:42:39 +0900

    元に戻しました

--- kaihatuSYOUSAI.php
+++ kaihatuSYOUSAI.php
@@ -91,10 +91,4 @@ integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jI
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
-        <script language="javascript" type="text/javascript">
-          const cart=document.getElementById('cart');
-          cart.Event('click', () =>{
-            btn.textContent ='カートから外す';
-          })
-        </script>
 </html>
