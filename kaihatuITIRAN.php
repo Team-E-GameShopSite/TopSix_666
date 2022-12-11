603ce2fe7368c6c374bbf2a0929ba898b8cd267f
mooonakira <moonfoot0301@gmail.com>
2022-12-11 23:34:02 +0900

    セール品にそれぞれ飛ぶようにしてます3

--- kaihatuITIRAN.php
+++ kaihatuITIRAN.php
@@ -39,9 +39,9 @@
         foreach ($searchArray as $row) {
           if($row['sale_flag'] == 1 && $row['sale_price'] != 0){
             echo "<div class='col-sm-9'>
-            <a href='kaihatuSYOUSAI_SALE.php?item_id=".$row['item_id']">
+            <a href='kaihatuSYOUSAI_SALE.php?item_id=".$row['item_id'].">
               <div class='card text-white'>
-              <img src=" . $row['image_path'] . "class='card-img' alt='...' height='80'>
+              <img src=". $row['image_path'] . "class='card-img' alt='...' height='80'>
               </div>
             </a>
             </div>";
