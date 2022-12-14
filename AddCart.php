d30534e98dea4edee8c8616c171269db053b824b
taketakebamboo <takeuguisu@gmail.com>
2022-12-15 00:59:10 +0900

    カート機能作成19

--- AddCart.php
+++ AddCart.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'dao/DBManager.php';
 $dbmnt = new DBManager();
-$dbmnt->AddCartById($_POST['user_id'], $_POST['item_id'], $_SESSION['suuryo']);
+$dbmnt->AddCartById($_SESSION['user_id'], $_POST['item_id'], $_SESSION['suuryo']);
 echo $_POST['item_id'] . " | ". $_POST['suuryo']. " | ". $_SESSION['user_id'];
 echo 'データベースインサート用ページです。';
 // header('Location: kaihatuCART.php');
