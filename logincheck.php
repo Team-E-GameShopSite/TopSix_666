c12241b65d6eca6ff784f2a5c4b9a42ecb280bae
taketakebamboo <takeuguisu@gmail.com>
2022-12-14 11:43:39 +0900

    ログイン処理の書き直しをしてみました。2

--- logincheck.php
+++ logincheck.php
@@ -6,7 +6,7 @@ foreach($userData as $row){
   $_SESSION['user_id'] = $row['user_id'];
   $_SESSION['user_name'] = $row['user_name'];
   $_SESSION['user_email'] = $row['email'];
-  header('Location: kaihatuTop.php')
+  header('Location: kaihatuTop.php');
 }
 if(count($userData) == 0){  header('Location: kaihatuLOGIN.php'); }
 ?>
