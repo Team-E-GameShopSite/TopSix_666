5026d7e31b8e6e80a519601aa528b1cd581b61eb
taketakebamboo <takeuguisu@gmail.com>
2022-12-15 00:51:04 +0900

    カート機能作成15

--- dao/DBManager.php
+++ dao/DBManager.php
@@ -145,8 +145,8 @@ class DBManager
 
     $ps = $pdo->prepare($spl);
     $ps->bindValue(1,(int)$item_id,PDO::PARAM_INT);
-    $ps->bindValue(2,$(int)item_id,PDO::PARAM_INT);
-    $ps->bindValue(3,$(int)user_id,PDO::PARAM_INT);
+    $ps->bindValue(2,(int)$item_id,PDO::PARAM_INT);
+    $ps->bindValue(3,(int)$user_id,PDO::PARAM_INT);
     $ps->bindValue(4,$date,PDO::PARAM_STR);
 
     var_dump((int)$item_id)."<br>";
