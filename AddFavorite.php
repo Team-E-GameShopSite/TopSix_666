129075add53ff60838a3a56f1b29198433d4c8bd
taketakebamboo <takeuguisu@gmail.com>
2022-12-15 21:55:58 +0900

    お気に入り機能作成4

--- AddFavorite.php
+++ AddFavorite.php
@@ -3,5 +3,6 @@ require_once 'dao/DBManager.php';
 $dbmnt = new DBManager();
 $dbmnt->AddFavoritesById($_SESSION['user_id'], $_POST['item_id']);
 echo '<h1>お気に入りに追加しました</h1>';
+echo $_POST['item_id'];
 // header('Location: kaihatuSYOSAI.php?item_id='.$_POST['item_id']);
 ?>
